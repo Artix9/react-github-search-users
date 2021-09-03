8634ef796cc58c067db776bcc944ae16e24af331
Artix9 <xxxxx01xxxxx@naver.com>
2021-09-04 00:54:18 +0900

    16 Fusion Charts - Doughnut Chart

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -18,7 +18,6 @@ const ChartComponent = ({ data }) => {
         theme: 'fusion',
         decimals: 0,
         pieRadius: '45%',
-        // paletteColors: '#f0db4f',
       },
       data,
     },
