cd85fc3f72d54a7285599d05e6e3eac2a5a46c5b
Artix9 <xxxxx01xxxxx@naver.com>
2021-09-04 22:42:23 +0900

    23 Search User

--- src/context/context.js
+++ src/context/context.js
@@ -18,6 +18,21 @@ const GithubProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState({ show: false, msg: '' });
 
+  const searchGithubUser = async (user) => {
+    toggleError();
+    // setLoading(true)
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) => {
+      console.log(err);
+    });
+    console.log(response);
+    if (response) {
+      setGithubUser(response.data);
+      // more logic here
+    } else {
+      toggleError(true, 'there is no user with that username');
+    }
+  };
+
   const checkRequests = () => {
     axios(`${rootUrl}/rate_limit`)
       .then(({ data }) => {
@@ -42,7 +57,14 @@ const GithubProvider = ({ children }) => {
 
   return (
     <GithubContext.Provider
-      value={{ githubUser, repos, followers, requests, error }}
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGithubUser,
+      }}
     >
       {children}
     </GithubContext.Provider>
