7a17b0d78053afbe3b0e28df2a9187b30c31b27b
Artix9 <xxxxx01xxxxx@naver.com>
2021-09-04 23:39:02 +0900

    24 Loading Spinner

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,7 +5,8 @@ import { GithubContext } from '../context/context';
 
 const Search = () => {
   const [user, setUser] = useState('');
-  const { requests, error, searchGithubUser } = useContext(GithubContext);
+  const { requests, error, searchGithubUser, isLoading } =
+    useContext(GithubContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -34,7 +35,9 @@ const Search = () => {
                 setUser(e.target.value);
               }}
             />
-            {requests > 0 && <button type='submit'>search</button>}
+            {requests > 0 && !isLoading && (
+              <button type='submit'>search</button>
+            )}
           </div>
         </form>
         <h3>requests : {requests} / 60</h3>
--- src/context/context.js
+++ src/context/context.js
@@ -15,12 +15,12 @@ const GithubProvider = ({ children }) => {
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
   const [requests, setRequests] = useState(0);
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState({ show: false, msg: '' });
 
   const searchGithubUser = async (user) => {
     toggleError();
-    // setLoading(true)
+    setIsLoading(true);
     const response = await axios(`${rootUrl}/users/${user}`).catch((err) => {
       console.log(err);
     });
@@ -31,6 +31,8 @@ const GithubProvider = ({ children }) => {
     } else {
       toggleError(true, 'there is no user with that username');
     }
+    checkRequests();
+    setIsLoading(false);
   };
 
   const checkRequests = () => {
@@ -64,6 +66,7 @@ const GithubProvider = ({ children }) => {
         requests,
         error,
         searchGithubUser,
+        isLoading,
       }}
     >
       {children}
